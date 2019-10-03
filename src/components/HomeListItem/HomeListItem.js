95d51854c2e3172b8d796e9763920496b1cd5ce0
Edith Emmings <emmings.edith@gmail.com>
2019-10-03 18:26:37 -0500

    created click handler to see movie details

--- src/components/HomeListItem/HomeListItem.js
+++ src/components/HomeListItem/HomeListItem.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class HomeListItem extends Component {
+    seeDetails = () => {
+        this.props.history.push('/movies/' + this.props.movie.id)
+    }
     render() {
         return (
             <div className="HomeListItem">
                 <h3>{this.props.movie.title}</h3>
-                <img src={this.props.movie.poster} />
+                <img 
+                    src={this.props.movie.poster} 
+                    alt={this.props.movie.title}
+                    onClick={seeDetails}
+                />
                 <p>{this.props.movie.description}</p>
             </div>
         );
