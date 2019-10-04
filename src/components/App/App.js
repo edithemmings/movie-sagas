46c4647e334c062bb6ff008f1b4bd51ecde77ba6
Edith Emmings <emmings.edith@gmail.com>
2019-10-03 22:23:53 -0500

    buttons on detail page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import HomeList from '../HomeList/HomeList'
 import Details from '../Details/Details'
 import Edit from '../Edit/Edit'
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 class Details extends Component {
     render() {
         return (
             <div className="Details">
+                <Link to='/'><button>Back to List</button></Link>
+                <Link to='/edit/1'><button>Edit</button></Link>
                 <h3>{this.props.reduxStore.movieDetails.title}</h3>
                 <img
                     src={this.props.reduxStore.movieDetails.poster}
